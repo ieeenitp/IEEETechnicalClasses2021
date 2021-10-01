6c7dc5214699175d4c667b74a56a261d51d07876
Manvi Jha <drprofmjha@gmail.com>
2021-10-02 00:50:52 +0530

    Create Question1.c

---
+++ 1-10-2021-Friday/Question1.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+int main()
+{
+int a=3 , b = 4, c;
+
+c=b-a;
+
+switch (c)
+
+{
+
+case 1||2:
+ printf ("God give me chance to change things\n");
+break;
+
+case a||b: 
+printf ("God give me a chance to run my show\n");
+
+break;
+
+}
+
+return 0;
